459310fe036cccb3b9e095b2414796c7938233cb
Mauricio Queiroz <mau.gomes@hotmail.com>
2021-03-11 16:20:14 -0300

    added new features as of 2nd assignm.

--- gui.py
+++ gui.py
@@ -1,30 +1,30 @@
+import sys 
 from image_processing import ProcesssedImage
-
-from tkinter import Tk, Label, Button, Canvas,Frame, NW, Toplevel, IntVar, Scale,HORIZONTAL, RIGHT,LEFT
+from tkinter import Tk, Label, Button, Canvas,Frame, NW, Toplevel, IntVar, Scale, Entry,HORIZONTAL, RIGHT,LEFT,simpledialog
 from PIL import ImageTk
-
+import matplotlib.pyplot as plt
+import numpy as np
 class ImageEditorGUI:
-    def __init__(self, master):
+    def __init__(self, master, filename):
+        master.title("Editor de Imagens")
         self.master = master
         
         self.rightframe = Frame(root)
         self.rightframe.pack( side = RIGHT )
         self.leftframe = Frame(root)
         self.leftframe.pack( side = LEFT )
+        self.left_right_frame = Frame(self.leftframe)
+        self.left_right_frame.pack( side = RIGHT )
 
-
-        self.workspace_image = ProcesssedImage("images/Gramado_22k.jpg")
+        self.workspace_image = ProcesssedImage(filename)
 
         self.label_original = Label(self.leftframe, text="Original Image")
         self.label_original.pack()
         self.update_image(update_original=True)
 
-        self.label = Label(self.leftframe, text="Processed Image")
+        self.label = Label(self.left_right_frame, text="Processed Image")
         self.label.pack()
         self.update_image()
-
-
-
 
         self.yflip_button = Button(self.rightframe, text="Horizontal Flip", command=self.yflip)
         self.yflip_button.pack()
@@ -32,20 +32,61 @@ class ImageEditorGUI:
         self.xflip_button = Button(self.rightframe, text="Vertical Flip", command=self.xflip)
         self.xflip_button.pack()
 
+        self.xflip_button = Button(self.rightframe, text="Rotate 90º", command=self.rotate)
+        self.xflip_button.pack()
+
         self.xflip_button = Button(self.rightframe, text="Greyscale", command=self.grey_scale)
         self.xflip_button.pack()
+
+        self.int_var = IntVar()
+        self.awnser = Entry(self.rightframe,width=5)
+        self.awnser.pack()
+
+        self.reset_button = Button(self.rightframe, text="Brightness", command=self.brightness)
+        self.reset_button.pack()
+
+        self.contrast_button = Button(self.rightframe, text="Contrast", command=self.contrast)
+        self.contrast_button.pack()
+
+        self.equalize_button = Button(self.rightframe, text="Equalize", command=self.equalize)
+        self.equalize_button.pack()
+
+        self.negative_button = Button(self.rightframe, text="Negative", command=self.negative)
+        self.negative_button.pack()
 
         self.var = IntVar()
         self.scale = Scale(self.rightframe, variable = self.var,to=255, orient=HORIZONTAL)
         self.scale.pack()
 
-        self.reset_button = Button(self.rightframe, text="Quantize", command=self.quantize_tones)
-        self.reset_button.pack()
+        self.quantize_button = Button(self.rightframe, text="Quantize", command=self.quantize_tones)
+        self.quantize_button.pack()
+
+        self.histogram_button = Button(self.rightframe, text="Histogram", command=self.histogram)
+        self.histogram_button.pack()
 
         self.reset_button = Button(self.rightframe, text="Reset", command=self.reset)
         self.reset_button.pack()
 
         self.close_button = Button(self.rightframe, text="Close", command=master.quit)
+        self.close_button.pack()
+
+        self.save_button = Button(self.rightframe, text="Save", command=self.save)
+        self.save_button.pack()
+
+        self.zoomout_button = Button(self.rightframe, text="Zoom Out", command=self.zoom_out)
+        self.zoomout_button.pack()
+
+        self.zoomin_button = Button(self.rightframe, text="Zoom In", command=self.zoom_in)
+        self.zoomin_button.pack()
+
+        self.conv_button = Button(self.rightframe, text="Convolution", command=self.conv)
+        self.conv_button.pack()
+
+        self.reset_button = Button(self.rightframe, text="Histogram Matching", command=self.matchhistogram)
+        self.reset_button.pack()
+        self.reset_button = Button(self.rightframe, text="Target Histogram", command=self.histogram_target)
+        self.reset_button.pack()
+        self.close_button = Button(self.rightframe, text="Load Target", command=self.loadtarget)
         self.close_button.pack()
 
 
@@ -57,8 +98,49 @@ class ImageEditorGUI:
         self.workspace_image.flip(vertical_axis=True)
         self.update_image()
 
+    def rotate(self):
+        self.workspace_image.rotate_90()
+        self.update_image()
+
     def grey_scale(self):
         self.workspace_image.grey_scale()
+        self.update_image()
+
+    def histogram(self):
+        hist_red, hist_green, hist_blue = self.workspace_image.get_histogram(normalized=True)
+        plt.bar(list(range(1,257)),hist_red)
+        plt.show()
+
+    def histogram_target(self):
+        hist_red, hist_green, hist_blue = self.workspace_image.get_histogram(target=True, normalized=True)
+        plt.bar(list(range(1,257)), hist_red,)
+        plt.show()
+
+    def loadtarget(self):
+        self.workspace_image.load_target("image_editor/images/Gramado_22k.jpg")
+
+    def matchhistogram(self):
+        self.workspace_image.match_histogram()
+        self.update_image()
+
+    def negative(self):
+        self.workspace_image.negative()
+        self.update_image()
+
+    def brightness(self):
+        self.workspace_image.brightness(int(self.awnser.get()))
+        self.update_image()
+
+    def equalize(self):
+        self.workspace_image.equalize()
+        self.update_image()
+
+    def contrast(self):
+        self.workspace_image.contrast(float(self.awnser.get()))
+        self.update_image()
+
+    def negative(self):
+        self.workspace_image.negative()
         self.update_image()
 
     def quantize_tones(self):
@@ -71,6 +153,36 @@ class ImageEditorGUI:
         self.master.update_idletasks()
         self.label.photo = self.processed_image
 
+    def zoom_out(self):
+        answer1 = simpledialog.askinteger("Input", "Sx",
+                                 parent=root,
+                                 minvalue=0, maxvalue=100)
+
+        answer2 = simpledialog.askinteger("Input", "Sy",
+                                 parent=root,
+                                 minvalue=0, maxvalue=100)
+
+        self.workspace_image.zoom_out(answer1,answer2)
+        self.update_image()
+
+    def conv(self):
+        answconv = simpledialog.askstring("Input", "3x3 filter separated by comma",
+                                 parent=root)
+
+        values = answconv.split(',')
+        values = [float(x) for x in values]
+
+        kernel_filter = np.array(values).reshape(3,3)
+
+        self.workspace_image.conv_filter(kernel_filter)
+        self.update_image()
+
+    def zoom_in(self):
+        self.workspace_image.zoom_in()
+        self.update_image()
+
+    def save(self):
+        self.workspace_image.save_image()
 
     def update_image(self, update_original: bool = False):
         self.processed_image = ImageTk.PhotoImage(image=self.workspace_image.processed_image)
@@ -87,6 +199,12 @@ class ImageEditorGUI:
     
 
 
+try:
+    filename =  str(sys.argv[1])
+    print("Opening:",filename) 
+except:
+    filename = "image_editor/images/Gramado_22k.jpg"
+
 root = Tk()
-gui = ImageEditorGUI(root)
+gui = ImageEditorGUI(root, filename)
 root.mainloop()
--- image_processing.py
+++ image_processing.py
@@ -8,10 +8,137 @@ class ProcesssedImage(object):
     def __init__(self, path_to_file: str):
             self.original_image = Image.open(path_to_file)
             self.processed_image = self.original_image.copy()
-
             self.px_data = np.asarray(self.processed_image).copy()
 
+    def brightness(self, brightness: int):
+        ''' Adds a brightness to the image
+        '''
+        image = self.px_data.copy()
+        image = image.astype('int')
+
+        red = image[:,:,0]
+        green = image[:,:,1]
+        blue = image[:,:,2]
+
+        red = red + brightness
+        green = green + brightness
+        blue = blue + brightness
+
+        image[:,:,0] = red
+        image[:,:,1] = green
+        image[:,:,2] = blue
+
+        image = np.where( image > 0, image, 0)
+        image = np.where( image < 254, image, 254)
+
+        # self.px_data = image
+        self.px_data = image.astype(np.uint8)
+        self.update_processed_image()
+
+    def contrast(self, contrast: int):
+        ''' Adds a brightness to the image
+        '''
+        image = self.px_data.copy()
+        image = image.astype('int')
+
+        red = image[:,:,0]
+        green = image[:,:,1]
+        blue = image[:,:,2]
+
+        red = red*contrast
+        green = green*contrast
+        blue = blue*contrast
+
+        image[:,:,0] = red
+        image[:,:,1] = green
+        image[:,:,2] = blue
+
+        image = np.where( image > 0, image, 0)
+        image = np.where( image < 254, image, 254)
+
+        # self.px_data = image
+        self.px_data = image.astype(np.uint8)
+        self.update_processed_image()
+
+    def equalize(self):
+        ''' Adds a brightness to the image
+        '''
         
+        height = len(self.px_data)
+        width = len(self.px_data[0])
+        red, *_ = self.get_histogram(grey_scale=True)
+
+        histogram_array = np.array(red)
+        cumulative_histogram = histogram_array.cumsum()
+        number_of_pixels = width*height
+        alpha = 255/number_of_pixels
+
+        cumulative_histogram = (cumulative_histogram*alpha).astype('int')
+
+
+        image = self.px_data.copy()
+
+        for i in range(height):
+            for j in range(width):
+                for c in range(3):
+                    px_value = image[i,j,c]
+                    eq_px_value = cumulative_histogram[px_value]
+                    image[i,j,c] = eq_px_value
+                    
+        # self.px_data = image
+        self.px_data = image
+        self.update_processed_image()
+
+    def match_histogram(self):
+        ''' Adds a brightness to the image
+        '''
+        
+
+        red, *_ = self.get_histogram(grey_scale=True)
+        red_target, *_ = self.get_histogram(grey_scale=True, target=True)
+
+        height = len(self.px_data)
+        width = len(self.px_data[0])
+        histogram_array = np.array(red)
+        cumulative_histogram = histogram_array.cumsum()
+        number_of_pixels = width*height
+        alpha = 255/number_of_pixels
+        cumulative_histogram = (cumulative_histogram*alpha).astype('int')
+
+        height_target = len(self.px_data)
+        width_target = len(self.px_data[0])
+        histogram_array_target = np.array(red_target)
+        cumulative_histogram_target = histogram_array_target.cumsum()
+        number_of_pixels_target = width_target*height_target
+        alpha_target = 255/number_of_pixels_target
+        cumulative_histogram_target = (cumulative_histogram_target*alpha_target).astype('int')
+
+        image = self.px_data.copy()
+
+        dict_tone = {}
+        for i in range(256):
+            eq_px_value = cumulative_histogram[i]
+            differences = [np.abs(eq_px_value-x) for x in cumulative_histogram_target]
+            match_px_val = int(np.argmin(differences))
+            dict_tone[i] = match_px_val
+
+        print(dict_tone)
+        for i in range(height):
+            for j in range(width):
+                for c in range(3):
+                    px_value = image[i,j,c]
+                    # eq_px_value = cumulative_histogram[px_value]
+
+                    #where in target eq_px_value is equal?
+                    # differences = [np.abs(eq_px_value-x) for x in cumulative_histogram_target]
+                    # match_px_val = np.argmin(differences)
+                    # image[i,j,c] = match_px_val
+                    image[i,j,c] = dict_tone[int(px_value)]
+                    
+        # self.px_data = image
+        self.px_data = image
+        self.update_processed_image()
+
     def save_image(self, file_name: str = 'output.jpg'):
         ''' Saves an image
 
@@ -19,11 +146,7 @@ class ProcesssedImage(object):
         file_name - str representing the filename, defaults to 
                     "output.jpg"
         '''
-
-        file_format = file_name.split('.')[1]
-        file_format = file_format.upper()
-
-        return self.processed_image.save(file_name, file_format)
+        return self.processed_image.save(file_name)
 
     def flip(self, vertical_axis: bool = False):
         ''' Flips an image in relation to a axis. Axis can be
@@ -49,15 +172,96 @@ class ProcesssedImage(object):
         self.px_data = np.array(flipped)
         self.update_processed_image()        
 
-    def grey_scale(self):
-        ''' Converts the rgb image to grayscale.
+    def get_histogram(self, normalized=False, grey_scale=False, target=False):
+        ''' 
+
+        Keyword Arguments:
+        normalized - .
         '''
 
-        image = self.px_data
+        if target:
+            gs_px_data = self.grey_scale(return_data=True, target=True)
+            image = np.asarray(Image.fromarray(gs_px_data)).copy()
+        else:
+            image = self.px_data
+            if grey_scale:
+                gs_px_data = self.grey_scale(return_data=True)
+                image = np.asarray(Image.fromarray(gs_px_data)).copy()
+            else:
+                image = np.asarray(self.processed_image).copy()
+
+        histogram_red = [0]*256
+        histogram_green = [0]*256
+        histogram_blue = [0]*256
+
+        for row in image:
+            for pixel in row:
+                histogram_red[pixel[0]] += 1
+                histogram_green[pixel[1]] += 1 
+                histogram_blue[pixel[2]] += 1
+                
+        #normalize
+        if normalized:
+            sum_red = sum(histogram_red)
+            sum_blue = sum(histogram_blue)
+            sum_green = sum(histogram_green)
+
+            histogram_red =[val/sum_red for val in histogram_red]
+            histogram_blue =[val/sum_blue for val in histogram_blue]
+            histogram_green =[val/sum_green for val in histogram_green]
+
+        return histogram_red, histogram_green, histogram_blue
+
+    def grey_scale(self, return_data=False, target=False):
+        ''' Converts the rgb image to grayscale.
+        '''
+        if target:
+            image = self.target_px_data
+        else:
+            image = self.px_data
+
         luminance_values_2D = np.array([self.luminance(row) for row in image]).astype(np.uint8)
         luminance_values_3D = np.repeat(luminance_values_2D[:, :, np.newaxis], 3, axis=2)
 
+        if return_data:
+            return luminance_values_3D
+
         self.px_data = luminance_values_3D
+        self.update_processed_image()
+
+    def load_target(self, file_name: str = 'output.jpg'):
+        ''' Saves an image
+
+        Keyword Arguments:
+        file_name - str representing the filename, defaults to 
+                    "output.jpg"
+        '''
+        self.target_original_image = Image.open(file_name)
+        self.target_processed_image = self.target_original_image.copy()
+        self.target_px_data = np.asarray(self.target_processed_image).copy()
+
+    def negative(self):
+        ''' Adds a brightness to the image
+        '''
+        image = self.px_data.copy()
+
+        red = image[:,:,0]
+        green = image[:,:,1]
+        blue = image[:,:,2]
+
+        red = 255-red
+        green = 255-green
+        blue = 255-blue
+
+        image[:,:,0] = red
+        image[:,:,1] = green
+        image[:,:,2] = blue
+
+        image = np.where( image > 0, image, 0)
+        image = np.where( image < 254, image, 254)
+
+        # self.px_data = image
+        self.px_data = image
         self.update_processed_image()
 
     def quantize_tones(self, max_tones: int = 255):
@@ -71,6 +275,98 @@ class ProcesssedImage(object):
         mapped_values_3D = np.repeat(mapped_values_2D[:, :, np.newaxis], 3, axis=2)
 
         self.px_data = mapped_values_3D
+        self.update_processed_image()
+
+    def zoom_out(self,sx,sy):
+        '''Performs zoom out in image
+        '''
+        img_height = self.px_data.shape[0]
+        img_width = self.px_data.shape[1]
+
+        s_width = int(np.ceil(img_width/sx))
+        s_height = int(np.ceil(img_height/sy))
+
+        s_matrix = np.zeros(s_width*s_height*3).reshape(s_height,s_width,3)
+
+        for c in range(3):
+            for ix in range(s_width):
+                for iy in range(s_height):
+                    s_matrix[iy,ix,c] = self.px_data[ iy*sy:(iy+1)*sy, ix*sx:(ix+1)*sx, c].mean()
+
+        s_matrix = s_matrix.astype(np.uint8)
+        self.px_data = s_matrix
+        self.update_processed_image()
+        
+    def zoom_in(self):
+        '''Magnifies 2x the image
+        '''
+        img_height = self.px_data.shape[0]
+        img_width = self.px_data.shape[1]
+
+        zin_width = int(img_width*2)
+        zin_height = int(img_height*2)
+
+        zin_matrix = np.zeros(zin_width*zin_height*3).reshape(zin_height,zin_width,3)
+
+        #place original values
+        for c in range(3):
+            for ix in range(img_width):
+                for iy in range(img_height):
+                    zin_matrix[iy*2,ix*2,c] = self.px_data[ iy, ix, c ]
+
+        # #interpolate col values
+        for c in range(3):
+            for ix in range(1,img_width-1):
+                for iy in range(1,img_height-1):
+                    zin_matrix[iy*2, (ix*2)-1 ,c] = (zin_matrix[ iy*2, (ix*2)-2, c] + zin_matrix[ iy*2, (ix*2), c])/2
+
+        # #interpolate row values
+        for c in range(3):
+            for ix in range(1,zin_width-1):
+                for iy in range(1,img_height-1):
+                    zin_matrix[(iy*2)-1, ix ,c] = (zin_matrix[ (iy*2)-2, ix, c] + zin_matrix[ iy*2, ix, c])/2
+
+        zin_matrix = zin_matrix.astype(np.uint8)
+        self.px_data = zin_matrix
+        self.update_processed_image()
+
+    def rotate_90(self):
+        ''' Roatate the image by 90 degrees. 
+        '''
+        img_height = self.px_data.shape[0]
+        img_width = self.px_data.shape[1]
+
+        r_matrix = np.zeros(img_width*img_height*3).reshape(img_width,img_height,3)
+
+        for c in range(3):
+            for ix in range(img_width):
+                for iy in range(img_height):
+                    r_matrix[ix,-iy,c] = self.px_data[iy,ix,c]
+        
+        self.px_data = r_matrix.astype(np.uint8)
+        self.update_processed_image()
+
+    def conv_filter(self, kernel_filter:np.array)->np.array:
+        '''Apply convolution filter in the image
+        '''
+        img_height = self.px_data.shape[0]
+        img_width = self.px_data.shape[1]
+
+        filtered_matrix = np.zeros(img_height*img_width*3).reshape(img_height,img_width,3)
+
+        #place original values
+        for c in range(3):
+            for ix in range(1,img_width-1):
+                for iy in range(1,img_height-1):
+                    window = (self.px_data[(iy-1):(iy+2),(ix-1):(ix+2),1]).astype(float)
+                    filtered_matrix[iy,ix,c] = (window*kernel_filter).sum()
+
+        filtered_matrix += 127
+        filtered_matrix = np.where( filtered_matrix > 0, filtered_matrix, 0)
+        filtered_matrix = np.where( filtered_matrix < 254, filtered_matrix, 254)
+
+        filtered_matrix = filtered_matrix.astype(np.uint8)
+        self.px_data = filtered_matrix
         self.update_processed_image()
 
     def update_processed_image(self):
